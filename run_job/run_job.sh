0b7d100680d6501cdcfd74c8ebcb45144b366407
Adnan Habib Rahin <adnan.rahin13@gmail.com>
2024-06-16 15:36:31 -0400

    update

--- run_job/run_job.sh
+++ run_job/run_job.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 SPARK_HOME=$SPARK_HOME
-APP_JAR="/home/rahin/source-code/spark/Spark-Flights-Data-Analysis/data-extract-processor/target/data-extract-processor-1.0-SNAPSHOT.jar"
-INPUT_PATH="/sandbox/storage/data/flight-data/data-set/"
-OUTPUT_PATH="/sandbox/storage/data/flight-data/filter_data/"
+APP_JAR="/home/rahin/source-code/spark/spark_id_cird_data_transformer/target/spark_ip_cidr_data_transformer-1.0-SNAPSHOT-jar-with-dependencies.jar"
+INPUT_PATH="/sandbox/storage/data/ip_cidr_data/dataset/ip_cidr_data_parquet"
+OUTPUT_PATH="/sandbox/storage/data/ip_cidr_data/filter_data/spark_extracted_data"
 PARTITIONS="2"
 
 $SPARK_HOME/bin/spark-submit \
     --master spark://dev-server01:7077 \
     --deploy-mode cluster \
-    --class org.flight.analysis.FlightDataProcessor \
-    --name FlightDataProcessorSpark \
+    --class oorg.data.transformer.IpCidrPIIDataExtractor \
+    --name IpCidrPIIDataExtractor \
     --driver-memory 4G \
     --driver-cores 4 \
     --executor-memory 8G \
